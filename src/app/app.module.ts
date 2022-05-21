7f4784381a13345f7c02184138a8580bba9c41b6
Marshall <mn.acunab@gmail.com>
2022-05-21 20:12:29 -0300

    refactoring style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { InputTextModule } from 'primeng/inputtext';
 import { TooltipModule } from 'primeng/tooltip';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -23,7 +25,9 @@ import { TableComponent } from './table/table.component';
     ButtonModule,
     ToolbarModule,
     InputTextModule,
-    TooltipModule
+    TooltipModule,
+    CardModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
